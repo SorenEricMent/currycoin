26499a8dbf619a01c7940e300a34b848b7ea0947
Yunchuan "Winslow" Hu <i@winsloweric.com>
2024-10-21 19:12:34 -0700

    Output helpers, working on MerkleTree PoC example

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -4,15 +4,36 @@ import Lib
 import Crypto.Hash.SHA256
 import qualified Data.ByteString as B
 
+import Data.Word (Word8)
+import Numeric (showHex)
+import Data.List (intercalate)
+
+
 type Hash = B.ByteString
 type Version = B.ByteString
-type Flag = B.ByteString    -- Constant of 0000 because we don't support SegWit
+type Flag = B.ByteString    
 type InCounter = Integer
 type OutCounter = Integer
 type Amount = Integer
 
+-- Constant section
+flagConst = B.pack [0, 0, 0, 1] -- Constant of 0000 because we don't support SegWit
+
+-- Helper function section
+byteStringToHex :: B.ByteString -> String
+byteStringToHex bs = intercalate "" $ map word8ToHex (B.unpack bs)
+word8ToHex :: Word8 -> String
+word8ToHex w = let hex = showHex w ""
+               in if length hex == 1 then '0' : hex else hex
+	       -- About helper is by ChatGPT, to create human readable hexdigit strings for better visuals.
+	       -- Note: Do NOT use those two function internally beside formatting output! All internal structures should be in ByteString
+	       
+-- Class definition section
 class Hashable a where
     hash :: a -> Hash
+
+instance Hashable B.ByteString where
+    hash = Crypto.Hash.SHA256.hash
 
 data TxInput = TxInput Hash    -- Previous spendable output
                
@@ -29,6 +50,14 @@ data Transaction = Transaction
 
 data MerkleTree a = INode Hash (MerkleTree a) (MerkleTree a) |
                     LeafNode Hash a
+
+createMerkleTreeFromList :: (Hashable a) => [a] -> a
+createMerkleTreeFromList = undefined
+
+addToMerkleTree :: (Hashable a) => (MerkleTree a) -> a -> (MerkleTree a)
+addToMerkleTree = undefined
+
+data MerkleTreeExample = MerkleTreeExample B.ByteString
 
 
 main :: IO ()
